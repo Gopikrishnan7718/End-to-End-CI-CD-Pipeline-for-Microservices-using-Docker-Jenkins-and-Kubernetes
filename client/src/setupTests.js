c9b3fa6b869404b9a47dc6467c44036b46ff3b09
Gopi <gopikrishnan.karuppaiah7718@gmail.com>
2026-04-24 16:22:17 +0530

    fix the errors

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const originalWarn = console.warn;
+
+beforeAll(() => {
+  console.warn = (...args) => {
+    const message = args.join(' ');
+
+    if (message.includes('React Router Future Flag Warning')) {
+      return;
+    }
+
+    originalWarn(...args);
+  };
+});
